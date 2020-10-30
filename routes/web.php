cdff70ba503cfe6e1b980e92255103475c7c8b56
NatanFigueiredo <natanfigueiredo27@gmail.com>
2020-10-30 04:43:30 -0300

    correções de layout e rotas

--- routes/web.php
+++ routes/web.php
@@ -31,15 +31,16 @@ Route::get('/principal',[TelaPrincipalController::class,'create']);
 Route::get('/pessoa',[PessoasController::class,'create']);
 Route::get('/pessoa/novo',[PessoasController::class,'createNew']);
 Route::post('/pessoa/novo',[PessoasController::class,'store'])->name('membro.novo');
-
 Route::get('/pessoa/editar/{id}',[PessoasController::class,'edit'])->name('membro.consultar');
 Route::post('/pessoa/editar/{id}',[PessoasController::class,'update'])->name('membro.editar');
 
 
 Route::get('/eleicoes',[EleicaosController::class,'create']);
+Route::post('/eleicoes',[EleicaosController::class,'store'])->name('eleicao.novo');
+Route::get('/eleicoes/edit/{id}',[EleicaosController::class,'edit'])->name('eleicao.consultar');
+Route::post('/eleicoes/edit/{id}',[EleicaosController::class,'update'])->name('eleicao.editar');
 
-Route::get('/eleicoes/novachapa',[ChapasController::class,'create']);
-
-
+Route::get('/eleicoes/novaChapa/{id}',[ChapasController::class,'create'])->name('eleicao.chapa');
+Route::post('/eleicoes/novaChapa/{id}',[ChapasController::class,'store'])->name('eleicao.salvarchapa');
 
 Route::get('/votacao',[VotacaosController::class,'create']);
